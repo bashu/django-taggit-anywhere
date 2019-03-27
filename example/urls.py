329ba2a438f064db629a7ba40454c34fdacd501d
Basil Shubin <basil.shubin@gmail.com>
2019-03-27 17:45:47 +0700

    support for django 2.x

--- example/urls.py
+++ example/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
